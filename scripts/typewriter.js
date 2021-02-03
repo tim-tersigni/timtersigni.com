9110575a0e6457e663450247063860644cd43f9e
Tim <tim.tersigni@gmail.com>
2021-02-03 14:26:07 -0600

    Change color scheme, fix responsive layouts

--- scripts/typewriter.js
+++ scripts/typewriter.js
@@ -1,12 +1,10 @@
+console.log('typewriter.js using observer');
+
 var txt = 
-'Seeking a programming internship in the Dallas or Austin area for the summer of 2021.<br/><br/>Contact me: <a href="mailto:timothy.tersigni.dev@gmail.com" target="_blank" style="color: white">timothy.tersigni.dev@gmail.com<\a> ';
+'Seeking a programming internship in the Dallas or Austin area for the summer of 2021. I am hardworking and always eager to learn! Please contact me with any opportunities.<br/><br/>Contact me: <a href="mailto:timothy.tersigni.dev@gmail.com" target="_blank" style="color: white">timothy.tersigni.dev@gmail.com<\a> ';
 var speed = 50;
 var i = 0;
 var inTag = false;
-
-console.log('typewriter.js loaded');
-typeWriter();
-
 function typeWriter () {
       if (i < txt.length) {
         document.getElementById('status').innerHTML = txt.substring(0, i) +'<span aria-hidden="true"></span>';
@@ -24,3 +22,12 @@ function typeWriter () {
           typeWriter();
       }
 }
+
+// Intersection Observer triggers typeWriter function when status is in view
+let options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: .3
+}
+let observer = new IntersectionObserver(typeWriter, options);
+observer.observe(document.getElementById('status'));
