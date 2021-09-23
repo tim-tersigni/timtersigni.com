8c3698667a915036d3779023cb6d6bb02b3ba529
Tim <tim.tersigni@gmail.com>
2021-09-22 22:03:15 -0500

    update for Junior year

--- scripts/typewriter.js
+++ scripts/typewriter.js
@@ -3,7 +3,7 @@ console.log('typewriter.js loaded');
 
 
 var txt = 
-'Seeking a programming internship in the Dallas or Austin area for the summer of 2021. I am hardworking and always eager to learn! Please contact me with any opportunities.<br/><br/>Contact me: <a href="mailto:timothy.tersigni.dev@gmail.com" target="_blank" style="color: white">timothy.tersigni.dev@gmail.com<\a> ';
+'Seeking a programming internship in the Dallas or Austin area. I am hardworking and always eager to learn! Please contact me with any opportunities.<br/><br/>Contact me: <a href="mailto:tim.tersigni@gmail.com" target="_blank" style="color: white">timothy.tersigni.dev@gmail.com<\a> ';
 var speed = 50;
 var i = 0;
 var inTag = false;
