9cd542c2c54d78896825ab6e050aa50c64231b8a
Tim <tim.tersigni@gmail.com>
2021-09-22 22:29:50 -0500

    fix email

--- scripts/typewriter.js
+++ scripts/typewriter.js
@@ -3,7 +3,7 @@ console.log('typewriter.js loaded');
 
 
 var txt = 
-'Seeking a programming internship in the Dallas or Austin area. I am hardworking and always eager to learn! Please contact me with any opportunities.<br/><br/>Contact me: <a href="mailto:tim.tersigni@gmail.com" target="_blank" style="color: white">timothy.tersigni.dev@gmail.com<\a> ';
+'Seeking a programming internship remotely or in the Dallas or Austin area. I am hardworking and always eager to learn! Please contact me with any opportunities.<br/><br/>Contact me: <a href="mailto:tim.tersigni@gmail.com" target="_blank" style="color: white">tim.tersigni.dev@gmail.com<\a> ';
 var speed = 50;
 var i = 0;
 var inTag = false;
