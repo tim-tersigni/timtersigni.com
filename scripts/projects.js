4ce5634341fcf730c59c3c22856e9a0ce42dabcb
Tim <tim.tersigni@gmail.com>
2022-09-20 21:41:17 -0500

    Add project links

--- scripts/projects.js
+++ scripts/projects.js
@@ -4,6 +4,10 @@ function makeTiles(numTiles, projects) {
   for (var i = 0; i < projects.length; i++) {
     const project = projects[i];
     
+    const projectLinkWrapper = document.createElement('a');
+    projectLinkWrapper.href = project.link;
+    projectLinkWrapper.className = "tile tile-link-wrapper"
+    
     const projectImageDiv = document.createElement('div');
     projectImageDiv.style.aspectRatio = '1';
     projectImageDiv.className = 'tile project-image-tile';
@@ -12,6 +16,7 @@ function makeTiles(numTiles, projects) {
     projectImage.src = project.image;
     projectImage.alt = `A picture of ${project.name}`
     projectImageDiv.appendChild(projectImage);
+    projectLinkWrapper.appendChild(projectImage);
 
     const projectDescDiv = document.createElement('div');
     projectDescDiv.style.aspectRatio = '1';
@@ -21,15 +26,14 @@ function makeTiles(numTiles, projects) {
     projectDesc.innerHTML = project.description;
     projectDescDiv.appendChild(projectDesc);
 
-
     if (i % 2 == 0) {
-      grid.appendChild(projectImageDiv);
+      grid.appendChild(projectLinkWrapper);
       grid.appendChild(projectDescDiv);
     }
     
     else {
       grid.appendChild(projectDescDiv);
-      grid.appendChild(projectImageDiv);
+      grid.appendChild(projectLinkWrapper);
     }
   }
 }
