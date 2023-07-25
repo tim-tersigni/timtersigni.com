f0dc51f66dcfe87c6bbd5711c73339e35c7de611
Tim Tersigni <tim.tersigni@gmail.com>
2023-07-24 20:05:45 -0500

    Improve descriptions further and change project link functionality

--- scripts/projects.js
+++ scripts/projects.js
@@ -7,6 +7,7 @@ function makeTiles(numTiles, projects) {
     const projectLinkWrapper = document.createElement('a');
     projectLinkWrapper.href = project.link;
     projectLinkWrapper.className = "tile tile-link-wrapper shrink-on-hover"
+    projectLinkWrapper.target = "_blank"
     
     const projectImageDiv = document.createElement('div');
     projectImageDiv.style.aspectRatio = '1';
